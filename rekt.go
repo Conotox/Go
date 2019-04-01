769f9de04dc15dd2cff6881d9d4d2366a30c8a51
TOMB <33571599+Conotox@users.noreply.github.com>
2019-04-01 15:22:02 +0100

    Update rekt.go
    random junk

--- rekt.go
+++ rekt.go
@@ -1,36 +1,33 @@
 package main
 
 import (
-"fmt"
-"bufio"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func scanner() {
-  scanner := bufio.NewScanner(os.Stdin)
-  for scanner.Scan() {
-    fmt.Println(scanner.Text())
-  }
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
 
-function otherGender(genderx string) {
-fmt.printLn("get riggedy rekt" + genderx)
-fmt.printLn("See we are all welcome here. We love you" + genderx)}
-
-function getRekt(gender string) {
-if gender == boi {
-	fmt.printLn("get riggedy rekt" + gender)
-} if gender == gurl {
-	fmt.printLn("get riggedy rekt" + gender)
-} else {
-	fmt.printLn("It's 2019 ffs we accept all genders what is your gender, child of God?")
-for scanner.Scan() {
-    otherGender(scanner.Text)
-  }
+func otherGender(genderx string) {
+	fmt.Println("get riggedy rekt " + genderx)
+	fmt.Println("See we are all welcome here. We love you" + genderx)
 }
 
-function main() {
-reader := bufio.NewReader(os.Stdin)
-for scanner.Scan() {
-    getRekt(scanner.Text)
-  }
+func getRekt(gender string) {
+	if gender == "boi" {
+		fmt.Println("get riggedy rekt " + gender)
+	} else if gender == "gurl" {
+		fmt.Println("get riggedy rekt " + gender)
+	} else {
+		fmt.Println("It's 2019 ffs we accept all genders so, get rekt " + gender)
+	}
+}
+
+func main() {
+	getRekt("boi / gurl / whatever")
 }
