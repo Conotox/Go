a744653bd43bc86a2a76fd70312d8fea9a9de514
TOMB <33571599+Conotox@users.noreply.github.com>
2019-04-01 13:26:05 +0100

    Create rekt.go

---
+++ rekt.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+"fmt"
+"bufio"
+)
+
+func scanner() {
+  scanner := bufio.NewScanner(os.Stdin)
+  for scanner.Scan() {
+    fmt.Println(scanner.Text())
+  }
+}
+
+function otherGender(genderx string) {
+fmt.printLn("get riggedy rekt" + genderx)
+fmt.printLn("See we are all welcome here. We love you" + genderx)}
+
+function getRekt(gender string) {
+if gender == boi {
+	fmt.printLn("get riggedy rekt" + gender)
+} if gender == gurl {
+	fmt.printLn("get riggedy rekt" + gender)
+} else {
+	fmt.printLn("It's 2019 ffs we accept all genders what is your gender, child of God?")
+for scanner.Scan() {
+    otherGender(scanner.Text)
+  }
+}
+
+function main() {
+reader := bufio.NewReader(os.Stdin)
+for scanner.Scan() {
+    getRekt(scanner.Text)
+  }
+}
